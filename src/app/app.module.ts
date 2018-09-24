bf02bf1c6da78d610d3956a3b5df9a282a789838
garyDav <gary.2810.dav@gmail.com>
2018-09-24 18:11:32 -0400

    generate user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,16 @@ import { MaterialModule } from './material.module';
 import { CashierComponent } from './cashier/cashier.component';
 import { CookComponent } from './cook/cook.component';
 import { WaiterComponent } from './waiter/waiter.component';
-import { AppRoutingModule } from './/app-routing.module'
+import { AppRoutingModule } from './/app-routing.module';
+import { UserComponent } from './user/user.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CashierComponent,
     CookComponent,
-    WaiterComponent
+    WaiterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
